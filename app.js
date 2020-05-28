dcc4ec1f1d2d6d3c8873d734f7721d43406d7170
我的征途是星辰大海 <1021690020@qq.com>
2020-05-28 11:05:38 +0800

    http 缓存

--- app.js
+++ app.js
@@ -1,48 +1,11 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const mime = require('mime');
-const fs = require('fs-extra');
-const Path = require('path');
-
-const app = new Koa();
-const router = new Router();
-
-// 处理首页
-router.get(/(^\/index(.html)?$)|(^\/$)/, async (ctx, next) => {
-    ctx.type = mime.getType('.html');
-
-    const content = await fs.readFile(Path.resolve(__dirname, './public/index.html'), 'UTF-8');
-    ctx.body = content;
-
-    await next();
-});
-
-// 处理 css 文件
-router.get(/\S*\.css$/, async (ctx, next) => {
-    const { response , path } = ctx;
-    ctx.type = mime.getType(path);
-	response.set('expires', new Date(Date.now() + 2 * 60 * 10000).toString());
-    const content = await fs.readFile(Path.resolve(__dirname, "public/"+path), 'UTF-8');
-    ctx.body = content;
-    await next();
-});
-
-// 处理 js 文件
-router.get(/\S*\.js$/, async (ctx, next) => {
-    const { response , path } = ctx;
-    ctx.type = mime.getType(path);
-    response.set('expires', new Date(Date.now() + 2 * 60 * 10000).toString());
-    const content = await fs.readFile(Path.resolve(__dirname, "public/"+path), 'UTF-8');
-    ctx.body = content;
-    await next();
-});
-
-app
-    .use(router.routes())
-    .use(router.allowedMethods());
-
-
+const express = require('express');
+const app = express();
+var options = { 
+  etag: false, // 禁用协商缓存
+  lastModified: false, // 禁用协商缓存
+  setHeaders: (res, path, stat) => {
+    res.set('Cache-Control', 'max-age=10000000'); // 强缓存超时时间为10秒
+  },
+};
+app.use(express.static((__dirname + '/public'), options));
 app.listen(3000);
-process.on('unhandledRejection', (err) => {
-    console.error('有 promise 没有 catch', err);
-});
--- public/static/js/13.4e83aaca0389713d375d.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([13],{"3syo":function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("\n    第三方接口管理\n")])},staticRenderFns:[]},r=t("VU/8")(null,l,!1,null,null,null);n.default=r.exports}});
